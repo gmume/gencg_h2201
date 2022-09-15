4ff93c45faf5817e5187a8f83d3b8efb1ab900d3
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2022-09-15 16:44:05 +0200

    video

--- content/day06/faces_v2_circles/face.js
+++ content/day06/faces_v2_circles/face.js
@@ -1,37 +1,36 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 let img;
+let resolution;
+let pixelIndex;
+let r;
+let g;
+let b;
+let avg;
 
 function preload() {
   img = loadImage("mela.png");
 }
 
 function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
   noLoop();
-  img.resize(50, 0);
-  createCanvas(img.width * 40, img.height * 40);
+  background(0);
+  stroke(255);
+  strokeWeight(1);
+  fill(255);
+  img.resize(60, 0);
+  img.loadPixels();
+  resolution = height / img.height;
 }
 
 function draw() {
-  background(0);
-
-  let w = width / img.width;
-  let h = height / img.height;
-  img.loadPixels();
   for (let i = 0; i < img.width; i++) {
     for (let j = 0; j < img.height; j++) {
-      const pixelIndex = (i + j * img.width) * 4;
-      const r = img.pixels[pixelIndex + 0];
-      const g = img.pixels[pixelIndex + 1];
-      const b = img.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 20;
-
-      stroke(255);
-      strokeWeight(1);
-      fill(255);
-      circle(i * w + w * 0.5, j * h + h * 0.5, avg);
-      console.log(i * w + w * 0.5, j * h + h * 0.5);
-
-
+      pixelIndex = (i + j * img.width) * 4;
+      r = img.pixels[pixelIndex + 0];
+      g = img.pixels[pixelIndex + 1];
+      b = img.pixels[pixelIndex + 2];
+      avg = (r + g + b) / 20;
+      circle(i * resolution + resolution / 2, j * resolution + resolution / 2, avg);
     }
   }
 }
--- content/day06/faces_v3_camera/face.js
+++ content/day06/faces_v3_camera/face.js
@@ -1,31 +1,31 @@
 let cam;
-const resolution = 4;
+let resolution = 4;
+let pixelIndex;
+let r;
+let g;
+let b;
+let avg;
 
 function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  frameRate(20);
   cam = createCapture(VIDEO);
-  console.log(cam);
   cam.hide();
-  createCanvas(cam.width*20, cam.height*25);
-  console.log(cam.width, cam.height);
+  noStroke();
+  fill(255);
 }
 
 function draw() {
   background(0);
-
-  let w = width / cam.width;
-  let h = height / cam.height;
   cam.loadPixels();
   for (let i = 0; i < cam.width; i += resolution) {
     for (let j = 0; j < cam.height; j += resolution) {
-      const pixelIndex = (i + j * cam.width) * 4;
-      const r = cam.pixels[pixelIndex + 0];
-      const g = cam.pixels[pixelIndex + 1];
-      const b = cam.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 20;
-
-      noStroke();
-      fill(255);
-      circle(i * w + w * 0.5, j * h + h * 0.5, avg);
+      pixelIndex = (i + j * cam.width) * 4;
+      r = cam.pixels[pixelIndex + 0];
+      g = cam.pixels[pixelIndex + 1];
+      b = cam.pixels[pixelIndex + 2];
+      avg = (r + g + b) / 3;
+      circle(i * resolution + 30, j * resolution + 30, avg / 15);
     }
   }
 }
