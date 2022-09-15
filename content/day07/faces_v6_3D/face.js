1f2d33f6600ec46382db2b943a6cf99a70f709f8
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2022-09-15 19:00:07 +0200

    camera

--- content/day07/faces_v6_3D/face.js
+++ content/day07/faces_v6_3D/face.js
@@ -1,5 +1,11 @@
 let img;
-
+let w;
+let h;
+let pixelIndex;
+let r;
+let g;
+let b;
+let avg;
 let offset;
 
 function preload() {
@@ -10,27 +16,26 @@ function setup() {
   img.resize(150, 0);
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   noStroke();
-  strokeWeight(1);
+  fill(255);
   offset = window.innerWidth/4;
-  camera(0, 0, offset * 4);
+  camera(0, 0, offset * 8);
 }
 
 function draw() {
   orbitControl();
   background(0);
 
-  let w = height / img.width;
-  let h = height / img.height * 1.5;
+  w = height / img.width;
+  h = height / img.height * 1.5;
   img.loadPixels();
   for (let i = 0; i < img.width; i++) {
     for (let j = 0; j < img.height; j++) {
-      const pixelIndex = (i + j * img.width) * 4;
-      const r = img.pixels[pixelIndex + 0];
-      const g = img.pixels[pixelIndex + 1];
-      const b = img.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 60;
+      pixelIndex = (i + j * img.width) * 4;
+      r = img.pixels[pixelIndex + 0];
+      g = img.pixels[pixelIndex + 1];
+      b = img.pixels[pixelIndex + 2];
+      avg = (r + g + b) / 60;
       
-      fill(255);
       push();
       translate((i * w + w * 0.5) - offset, (j * h + h * 0.5) - offset * 2, avg * 80);
       sphere(avg, 8, 8);
